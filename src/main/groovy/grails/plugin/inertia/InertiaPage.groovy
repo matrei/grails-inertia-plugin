7dff9a362e41b3a0b8815dfde6cfd26a69ff028d
Mattias Reichel <mattias.reichel@gmail.com>
2022-12-21 15:16:01 +0100

    Added POGO to represent the data in inertia responses.

---
+++ src/main/groovy/grails/plugin/inertia/InertiaPage.groovy
@@ -0,0 +1,13 @@
+package grails.plugin.inertia
+
+import groovy.transform.Immutable
+
+@Immutable
+class InertiaPage {
+
+    String component
+    Map props
+    String url
+    String version
+
+}
