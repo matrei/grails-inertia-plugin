05aec5d3108949c270f90b485a9939d050f644b4
Mattias Reichel <mattias.reichel@gmail.com>
2022-12-21 15:22:11 +0100

    Removed the redirectInertia method from the InertiaTrait. Added documentation to the locationInertia method in InertiaTrait.

--- src/main/groovy/grails/plugin/inertia/InertiaTrait.groovy
+++ src/main/groovy/grails/plugin/inertia/InertiaTrait.groovy
@@ -3,11 +3,29 @@ package grails.plugin.inertia
 import grails.artefact.Enhances
 import org.springframework.web.servlet.ModelAndView
 
+@SuppressWarnings('unused')
 @Enhances(['Controller','Interceptor'])
 trait InertiaTrait {
 
     ModelAndView renderInertia(String component, Map props = [:], Map viewData = [:]) {
         Inertia.render component, props, viewData
+    }
+
+    /**
+     * External redirects
+     *
+     * Sometimes it's necessary to redirect to an external website, or even
+     * another non-Inertia endpoint in your app, within an Inertia request.
+     * This is possible using a server-side initiated window.location visit.
+     *
+     * This will generate a 409 Conflict response, which includes the destination
+     * URL in the X-Inertia-Location header. Client-side, Inertia will detect this
+     * response and automatically dow a window.location = url visit.
+     *
+     * @param url The url to redirect to
+     */
+    void locationInertia(String url) {
+        Inertia.location url
     }
 
     Map getInertiaSharedData() {
