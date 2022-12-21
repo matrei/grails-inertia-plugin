682b4f7a255be1ac0ff3ee4b83247787ad68d20f
Mattias Reichel <mattias.reichel@gmail.com>
2022-12-21 15:11:45 +0100

    Added test for Inertia view rendering.

---
+++ src/test/groovy/grails/plugin/inertia/InertiaViewSpec.groovy
@@ -0,0 +1,23 @@
+package grails.plugin.inertia
+
+import grails.plugin.json.view.test.JsonViewTest
+import spock.lang.Specification
+
+class InertiaViewSpec extends Specification implements JsonViewTest {
+
+    void "Test render a raw JSON view"() {
+
+        given: "An Inertia page"
+        def inertiaPage = new InertiaPage(component: 'HelloWorld', props: [name: 'Mattias'], url: '/helloworld', version: '1')
+
+        when: "A json view is rendered"
+        def result = render view: '/inertia/json', model: [inertiaPage: inertiaPage]
+
+        then: "The json is correct"
+        result.json.component == 'HelloWorld'
+        result.json.props.name == 'Mattias'
+        result.json.url == '/helloworld'
+        result.json.version == '1'
+
+    }
+}
