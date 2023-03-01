370197832448341128c9d73a0f2496aeb5e21c78
Mattias Reichel <mattias.reichel@gmail.com>
2023-03-01 15:00:28 +0100

    Added license header and author to source files.

--- grails-app/controllers/grails/plugin/inertia/InertiaInterceptor.groovy
+++ grails-app/controllers/grails/plugin/inertia/InertiaInterceptor.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022-2023 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package grails.plugin.inertia
 
 import grails.config.Config
@@ -16,6 +31,12 @@ import static Inertia.INERTIA_ATTRIBUTE_MANIFEST
 import static Inertia.INERTIA_ATTRIBUTE_VERSION
 import static grails.web.http.HttpHeaders.VARY
 
+/**
+ * Interceptor that implements Inertia functionality.
+ *
+ * @author Mattias Reichel
+ * @since 1.0.0
+ */
 @CompileStatic
 class InertiaInterceptor implements GrailsConfigurationAware, GrailsApplicationAware {
 
--- src/main/groovy/grails/plugin/inertia/InertiaGrailsPlugin.groovy
+++ src/main/groovy/grails/plugin/inertia/InertiaGrailsPlugin.groovy
@@ -1,8 +1,29 @@
+/*
+ * Copyright 2022-2023 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package grails.plugin.inertia
 
 import grails.plugins.Plugin
 import groovy.transform.CompileStatic
 
+/**
+ * Grails plugin descriptor class.
+ *
+ * @author Mattias Reichel
+ * @since 1.0.0
+ */
 @SuppressWarnings("unused")
 @CompileStatic
 class InertiaGrailsPlugin extends Plugin {
