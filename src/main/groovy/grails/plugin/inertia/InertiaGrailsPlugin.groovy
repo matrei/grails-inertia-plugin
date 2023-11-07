4e6ec717f096dec2a860ab2369fbf1be85708c5a
Mattias Reichel <mattias.reichel@gmail.com>
2023-11-07 14:36:52 +0100

    Update the Plugin descriptor to minimum Grails version 6

--- src/main/groovy/grails/plugin/inertia/InertiaGrailsPlugin.groovy
+++ src/main/groovy/grails/plugin/inertia/InertiaGrailsPlugin.groovy
@@ -28,13 +28,12 @@ import groovy.transform.CompileStatic
 @CompileStatic
 class InertiaGrailsPlugin extends Plugin {
 
-    def grailsVersion = "5.0.1 > *"
+    def grailsVersion = "6.0.0 > *"
     def pluginExcludes = []
     def title = "Grails Adapter for Inertia.js"
     def author = "Mattias Reichel"
     def authorEmail = "mattias.reichel@gmail.com"
     def description = "Inertia server-side adapter for Grails"
-    def profiles = ['web']
 
     def documentation = "https://github.com/matrei/inertia-grails-plugin#readme"
     def license = "APACHE"
