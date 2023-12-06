d9e45931134e4343bf89fb116226f41c5ad1ff54
Mattias Reichel <mattias.reichel@gmail.com>
2023-12-06 16:49:05 +0100

    Make sure the SSR server is started before leaving bootstrap stage. Integration tests are failing trying to access the SSR server before it is up.

--- grails-app/init/grails/plugin/inertia/Bootstrap.groovy
+++ grails-app/init/grails/plugin/inertia/Bootstrap.groovy
@@ -61,11 +61,37 @@ class BootStrap {
         }
 
         ssrProcess = new ProcessBuilder().inheritIO().command('node',  bundle).start()
-        log.debug 'SSR process started with pid: ' + ssrProcess.pid()
+        def url = new URL(grailsApplication.config.getProperty("${ServerSideRenderConfig.PREFIX}.url", String))
+        waitForSsrServer(url.host, url.port)
+        log.debug "SSR process started with pid: ${ssrProcess.pid()}"
     }
 
     void stopSSR() {
-        log.debug 'Stopping SSR process with pid: ' + ssrProcess?.pid()
+        log.debug "Stopping SSR process with pid: ${ssrProcess?.pid()}"
         ssrProcess?.destroy()
+    }
+
+    void waitForSsrServer(String host, int port, int timeoutMs = 1000, int maxRetries = 10) {
+        boolean portOpen = false
+        int tryNumber = 0
+        while (!portOpen && ++tryNumber <= maxRetries) {
+            log.debug "Checking if SSR server is up on $host:$port ($tryNumber/$maxRetries)..."
+            try (Socket socket = new Socket(host, port)) {
+                // If the socket is successfully created, the port is open
+                log.debug "SSR server is up!"
+                portOpen = true
+            } catch (IOException ignore) {
+                // Port is not open yet, wait for some time before retrying
+                log.debug "SSR server is not responding yet, retrying in $timeoutMs ms..."
+                try {
+                    Thread.sleep(timeoutMs)
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace()
+                }
+            }
+        }
+        if (!portOpen) {
+            throw new IllegalStateException("SSR server is not responding on $host:$port")
+        }
     }
 }
