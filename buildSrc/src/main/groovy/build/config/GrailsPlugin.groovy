c16a9f2ce6609ee85b869db213797529c1617354
Mattias Reichel <mattias.reichel@gmail.com>
2026-02-23 10:33:46 +0100

    refactor: simplify gradle plugin application logic

--- buildSrc/src/main/groovy/build/config/GrailsPlugin.groovy
+++ buildSrc/src/main/groovy/build/config/GrailsPlugin.groovy
@@ -12,25 +12,33 @@ import org.grails.gradle.plugin.views.json.GsonViewCompilerTask
 @CompileStatic
 class GrailsPlugin implements Plugin<Project>, GrailsProject {
 
+    private static final List<String> PLUGINS = [
+        'org.apache.grails.gradle.grails-plugin',
+        'org.apache.grails.gradle.grails-gson',
+        'org.apache.grails.gradle.grails-gsp',
+        'build.config.java',
+        'build.config.publish',
+        'build.config.reproducible',
+        'build.config.test'
+    ]
+
     @Override
     void apply(Project project) {
+
         configureProjectVersion(project)
         configureGrailsVersion(project)
 
-        // This prevents the Grails Gradle Plugin from unnecessarily excluding slf4j-simple in the generated POMs
+        // This prevents the Grails Gradle Plugin from unnecessarily
+        // excluding slf4j-simple in the generated POMs:
         // https://github.com/grails/grails-gradle-plugin/issues/222
         project.extensions.extraProperties.set(
                 'slf4jPreventExclusion',
                 'true'
         )
 
-        project.pluginManager.apply('org.apache.grails.gradle.grails-plugin')
-        project.pluginManager.apply('org.apache.grails.gradle.grails-gson')
-        project.pluginManager.apply('org.apache.grails.gradle.grails-gsp')
-        project.pluginManager.apply('build.config.java')
-        project.pluginManager.apply('build.config.publish')
-        project.pluginManager.apply('build.config.reproducible')
-        project.pluginManager.apply('build.config.test')
+        PLUGINS.each {
+            project.pluginManager.apply(it)
+        }
 
         project.extensions.configure(GrailsExtension) {
             it.springDependencyManagement = false
